c2c65e930f25ad916042836f0cedd3900042587c
rugkfl <kimgyoungha61@gmail.com>
2023-12-04 16:47:40 +0900

    파이썬 실습 제출

--- docs/quests/onehandgame.py
+++ docs/quests/onehandgame.py
@@ -1,14 +1,26 @@
-str_first = "1"
+str_first = "1" 
 str_second = ""
 str_third = "2"
 str_fourth = ""
 str_fifth = "3"
-print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
-print("{} , {} , {} , {}, {}".format(str_second, str_first, str_third, str_fourth, str_fifth))
-print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_first, str_third, str_fifth))
-print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_first, str_third, str_fifth))
-print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_third, str_first, str_fifth))
-print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_third, str_fifth, str_first))
-print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_fifth, str_third, str_first))
-print("{} , {} , {} , {}, {}".format(str_second, str_fifth, str_fourth, str_third, str_first))
-print("{} , {} , {} , {}, {}".format(str_fifth, str_second, str_third, str_fourth, str_first))
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # 1, - ,2, - ,3
+str_first, str_second = str_second, str_first
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) #  -, 1, 2, - ,3
+str_third, str_fourth = str_fourth, str_third
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, 1, -, 2, 3
+str_second, str_third = str_third, str_second
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, -, 1, 2, 3
+str_third, str_fourth = str_fourth, str_third
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, -, 2, 1, 3
+str_fourth, str_fifth = str_fifth, str_fourth
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, -, 2, 3, 1
+str_fourth, str_third = str_third, str_fourth
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, -, 3, 2, 1
+str_third, str_second = str_second, str_third
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, 3, -, 2, 1
+str_fourth, str_third = str_third, str_fourth
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # -, 3, 2, -, 1
+str_second, str_first = str_first, str_second
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth)) # 3, -, 2, -, 1
+
+
