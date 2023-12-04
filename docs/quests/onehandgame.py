296c15de014db73110ab06630c6924963aafcc5c
rugkfl <kimgyoungha61@gmail.com>
2023-12-04 16:18:02 +0900

    파이썬 실습

---
+++ docs/quests/onehandgame.py
@@ -0,0 +1,14 @@
+str_first = "1"
+str_second = ""
+str_third = "2"
+str_fourth = ""
+str_fifth = "3"
+print("{} , {} , {} , {}, {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+print("{} , {} , {} , {}, {}".format(str_second, str_first, str_third, str_fourth, str_fifth))
+print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_first, str_third, str_fifth))
+print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_first, str_third, str_fifth))
+print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_third, str_first, str_fifth))
+print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_third, str_fifth, str_first))
+print("{} , {} , {} , {}, {}".format(str_second, str_fourth, str_fifth, str_third, str_first))
+print("{} , {} , {} , {}, {}".format(str_second, str_fifth, str_fourth, str_third, str_first))
+print("{} , {} , {} , {}, {}".format(str_fifth, str_second, str_third, str_fourth, str_first))
