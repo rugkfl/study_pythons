4d1ce9f0e5fd81baa81330c315e36a09f919b8e4
rugkfl <kimgyoungha61@gmail.com>
2023-12-05 12:47:44 +0900

    수정

--- docs/quests/inputwiththree_outputcuboid.py
+++ docs/quests/inputwiththree_outputcuboid.py
@@ -5,6 +5,7 @@
 str_input = input("가로 세로 높이 :")
 str_first, str_second, str_third = "4 1 1".split()
 num_first, num_second, num_third = int(str_first), int(str_second), int(str_third)
-print("가로({})m * 세로({})m * 높이({})m = 직육면체(4)m^3".format(num_first, num_second, num_third))
+cuboid = num_first * num_second * num_third
+print("가로({})m * 세로({})m * 높이({})m = 직육면체({})m^3".format(num_first, num_second, num_third, cuboid))
 
 
