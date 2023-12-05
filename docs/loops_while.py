4a2202a98cb33e8b869f9d8cdba7133b90e34227
rugkfl <kimgyoungha61@gmail.com>
2023-12-05 18:02:45 +0900

    파이썬

--- docs/loops_while.py
+++ docs/loops_while.py
@@ -3,10 +3,26 @@
 #     pass
 
 num_virtual = 0
-while True :
+# while 정지 1 case with break문 사용
+# while True :
+#     pass
+#     num_virtual = num_virtual + 1
+#     print("{} = num_virtual + 1".format(num_virtual))
+#     if num_virtual >=5 :
+#         pass
+#         break
+#     pass
+
+
+# while 정지 2 case without break문 사용
+num_virtual = 0
+while num_virtual < 5 :
     pass
     num_virtual = num_virtual + 1
     print("{} = num_virtual + 1".format(num_virtual))
     pass
 
+
+
 # 풀기 : 5 구구단 결과 매번 출력
+print("End_Program!")
---
+++ docs/quests/timestables.py
@@ -0,0 +1,7 @@
+gugudan = 0
+
+while gugudan < 9:
+    pass
+    gugudan = gugudan + 1
+    print("5 * {} = {}".format(gugudan, gugudan*5))
+    pass
