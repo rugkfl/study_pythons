8856a842c5c30c6a6555f26bb1f6aafa69ab7175
rugkfl <kimgyoungha61@gmail.com>
2023-12-06 11:48:09 +0900

    수정

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -1,6 +1,6 @@
 num_multiply = int(input("단수 : "))
-gugudan = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+gugudan = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 for num_gugudan in gugudan :
     pass
-    print("5 * {} = {}".format(num_gugudan, num_gugudan*5))
+    print("{} * {} = {}".format(num_multiply,num_gugudan, num_multiply*num_gugudan))
 
