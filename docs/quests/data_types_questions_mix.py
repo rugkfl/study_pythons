ba6928ef76cd22a66c2ee808b1af2b1b22269aff
rugkfl <kimgyoungha61@gmail.com>
2023-12-12 18:20:07 +0900

    실습

--- docs/quests/data_types_questions_mix.py
+++ docs/quests/data_types_questions_mix.py
@@ -1,3 +1,4 @@
+'''
 def question_and_answer() :
     mixed_questions = [
     {
@@ -9,10 +10,10 @@ def question_and_answer() :
     ]
     return mixed_questions
 
-mixed_questions=question_and_answer()
+mixed_questions = question_and_answer()
 
 temp_list = []
-
+pass
 for j in range(3) :
     for dict_i in range(len(mixed_questions)) :
         mixed_questions[dict_i]["question"] = input("question: ")
@@ -22,8 +23,41 @@ for j in range(3) :
         temp_list.append(mixed_questions)
 
 
+
+print(temp_list)
+'''
+
+def question_and_answer():
+    mixed_questions = []
+    for j in range(3):
+        question = input("question: ")
+        answer = input("answer: ")
+        correct_index = int(input("correct_index: "))
+        score = int(input("score: "))
+
+        mixed_questions.append({
+            "question": question,
+            "answer": answer,
+            "correct_index": correct_index,
+            "score": score
+        })
+
+    return mixed_questions
+
+temp_list = question_and_answer()
 print(temp_list)
 
 
 
-
+# for j in range(3) :
+#     pass
+#     for dict_i in range(len(mixed_questions)) :
+#         pass
+#         mixed_questions[dict_i]["question"] = input("question: ")
+#         pass
+#         mixed_questions[dict_i]["answer"] = input("answer : ")
+#         pass
+#         mixed_questions[dict_i]["correct_index"] = int(input("correct_index : "))
+#         pass
+#         mixed_questions[dict_i]["score"] = int(input("score : "))
+#         temp_list.append(mixed_questions)
