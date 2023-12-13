5b02d98cf1bede638f106c9f1721565a90a0e595
rugkfl <kimgyoungha61@gmail.com>
2023-12-13 09:23:31 +0900

    수정

--- docs/quests/data_types_questions_mix.py
+++ docs/quests/data_types_questions_mix.py
@@ -1,33 +1,4 @@
-'''
-def question_and_answer() :
-    mixed_questions = [
-    {
-        "question":"",
-        "answer": [],
-        "correct_index": 0,
-        "score": 0
-    }
-    ]
-    return mixed_questions
-
-mixed_questions = question_and_answer()
-
-temp_list = []
-pass
-for j in range(3) :
-    for dict_i in range(len(mixed_questions)) :
-        mixed_questions[dict_i]["question"] = input("question: ")
-        mixed_questions[dict_i]["answer"] = input("answer : ")
-        mixed_questions[dict_i]["correct_index"] = int(input("correct_index : "))
-        mixed_questions[dict_i]["score"] = int(input("score : "))
-        temp_list.append(mixed_questions)
-
-
-
-print(temp_list)
-'''
-
-
+# 해당 코드만 유효
 mixed_questions = [
     {
         "question":"",
@@ -57,6 +28,39 @@ def question_and_answer():
 mixed_questions = question_and_answer()
 print(mixed_questions)
 
+
+
+
+
+# 메모장
+'''
+def question_and_answer() :
+    mixed_questions = [
+    {
+        "question":"",
+        "answer": [],
+        "correct_index": 0,
+        "score": 0
+    }
+    ]
+    return mixed_questions
+
+mixed_questions = question_and_answer()
+
+temp_list = []
+pass
+for j in range(3) :
+    for dict_i in range(len(mixed_questions)) :
+        mixed_questions[dict_i]["question"] = input("question: ")
+        mixed_questions[dict_i]["answer"] = input("answer : ")
+        mixed_questions[dict_i]["correct_index"] = int(input("correct_index : "))
+        mixed_questions[dict_i]["score"] = int(input("score : "))
+        temp_list.append(mixed_questions)
+
+
+
+print(temp_list)
+'''
 
 
 # for j in range(3) :
