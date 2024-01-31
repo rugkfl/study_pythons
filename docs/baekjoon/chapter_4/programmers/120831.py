0bd867531446eb97dd1c040b0b6b7a8c508ccbed
rugkfl <kimgyoungha61@gmail.com>
2024-01-31 17:01:44 +0900

    update

--- docs/baekjoon/chapter_4/programmers/120831.py
+++ docs/baekjoon/chapter_4/programmers/120831.py
@@ -1,8 +1,10 @@
-n = 10 
-def solution(n) :
+n = 10
+def solution(n):
     n_list = []
     for i in range(n) :
-        i += 2
-        n_list.append(i)
-    return n_list
+        i += 1
+        if i % 2 == 0:
+            n_list.append(i)
+    answer = sum(n_list)
+    return answer
 print(solution(n))
