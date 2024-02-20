28b7f970739d74c1a6999d174b139e83cbdfba80
rugkfl <kimgyoungha61@gmail.com>
2024-02-20 17:51:12 +0900

    update

--- docs/coding_test_programmers/120851.py
+++ docs/coding_test_programmers/120851.py
@@ -3,9 +3,30 @@
 # 문제 설명
 # 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 
+# my_string = "1a2b3c4d123"
+# num_list = []
+# for i in my_string :
+#     try :  # 반복문을 통해 int로 변환이 가능한 문자열만 리스트에 append
+#         int(i)
+#         num_list.append(i)
+#     except :  # 에러가 나면 통과해서 다시 처음으로
+#         continue
+# int_list = list(map(int, num_list))  # list의 문자열을 int로 바꿔줌
+# result = sum(int_list)
+# print(result)
+
+
+# my_string = "1a2b3c4d123"
 my_string = "aAb1B2cC34oOp"
-num_list = []
-for i in my_string :
-    if type(my_string) == int :
-        num_list.append(my_string[i])
-print(num_list)
+def solution(my_string):
+    num_list = []
+    for i in my_string :
+        try :
+            int(i)
+            num_list.append(i)
+        except :
+            continue
+    int_list = list(map(int, num_list))
+    answer = sum(int_list)
+    return answer
+print(solution(my_string))
